0758df67070c93f947c647c7038fda56bfe58eb4
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-11-20 13:10:49 +0600

    first commit

---
+++ for-loop.js
@@ -0,0 +1,93 @@
+// for (var number = 0; number < 8; number++) {
+//   // console.log("Hello")
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   // console.log(i)
+// }
+
+// console.log("for loop practice");
+
+// for (var i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+// for (var i = 0; i <= 10; i+=2){
+//     console.log(i)
+// }
+// for (var i = 1; i <= 10; i+=2){
+//     console.log(i)
+// // }
+
+// var computer = ["pc", "display", "keyboard", "mous", "cable", "ups"];
+
+// for (var i = 0; i < computer.length; i++) {
+//   var cpu = computer[i];
+//   // console.log(cpu)
+// }
+
+// for (var i = 1; i < 100; i += 2) {
+//   // console.log(i)
+// }
+
+// var numbers = [
+//   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+// // console.log(numbers);
+
+// // for (var i = 0; i < numbers.length; i++) {
+// //   var number = numbers[i];
+// //   if (number > 5 && number < 15) {
+// //     console.log(number);
+// //   }
+// // }
+
+// for (var i = 0; i < numbers.length; i++) {
+//   var number = numbers[i];
+
+// // console.log(numbers)
+
+// // var beasts = ["ant", "bison", "camel", "duck", "bison", "mal", "bison"];
+// // console.log(beasts.indexOf("bison"));
+
+// // for (var i = 0; i <= 100000000000; i++) {
+// //   console.log(i, ": ajke amar mon valo nei");
+// }
+
+// var numbers = 0;
+// while (numbers <= 100) {
+//   console.log(numbers, ": ajke amar mon valo nei");
+//   numbers++;
+// }
+
+// var arr = [];
+// for (var i = 412; i < 456; i++) {
+//   if (i % 2 === 0) {
+//     arr.push(i);
+//     // console.log(i);
+//   }
+// }
+// console.log(arr);
+
+// var num = ["a", "b", "c", "d", "e", "f"];
+// console.log(num[2])
+
+// for (var i = 0; i < num.length; i++) {
+//   console.log(num[i])
+// // }
+
+// num.map(item=>console.log(item))
+
+// for (var i = 30; i < 86; i++) {
+//   if (i > 44) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+var prices = [100, 300, 500, 50, 80, 190, 600, 30];
+for (var i = 0; i < prices.length; i++) {
+  var price = prices[i];
+  if (price < 200) {
+    console.log(price);
+  }
+}
---
+++ while-loop.js
@@ -0,0 +1,21 @@
+var appleGiven = 0;
+
+while (appleGiven < 10) {
+  console.log("please gentolmane take your apple & take your drink");
+  console.log(appleGiven);
+  appleGiven += 1;
+}
+
+var misty = 0;
+while (misty < 10) {
+  console.log("ei lon apnar misty");
+  console.log(misty);
+  misty++;
+}
+
+var taka = 0;
+while (taka < 10) {
+  console.log("ei lon apnar vager taka apni deikkha tar por buijja lon");
+  console.log(taka);
+  taka++;
+}
